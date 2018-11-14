40b2a64cafe6ae9fd78b3a66ebf9f88a69c580df
Ben Kraus <ben@betatrain.io>
2018-11-14 16:25:59 -0700

    Fetch single beta tester

--- lib/app_store_connect_api/testflight/beta_testers.rb
+++ lib/app_store_connect_api/testflight/beta_testers.rb
@@ -1,9 +1,17 @@
 module AppStoreConnectAPI
   module TestFlight
     module BetaTesters
+
+      # Find and list beta testers for all apps, builds, and beta groups.
       def beta_testers
         self.class.get("/betaTesters", headers: self.headers)
       end
+
+      # Get a specific beta tester.
+      def beta_tester(id)
+        self.class.get("/betaTesters/#{id}", headers: self.headers)
+      end
+      
     end
   end
 end
